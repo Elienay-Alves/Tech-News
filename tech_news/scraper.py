bf20eaf9d4c1736d5941b8a69b973c406341db1f
Elienay Alves <elienay8486@gmail.com>
2023-04-21 18:15:27 -0300

    Requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,22 @@
+import time
+import requests
+
+
 # Requisito 1
-def fetch(url):
-    """Seu código deve vir aqui"""
+def fetch(url=None):
+    if url is None:
+        return None
+
+    time.sleep(1)
+    try:
+        result = requests.get(
+            url, timeout=3, headers={"user-agent": "Fake user-agent"}
+        )
+        result.raise_for_status()
+        return result.text
+    except requests.exceptions.RequestException as err:
+        print(f"Erro ao obter a página {url}: {err}")
+        return None
 
 
 # Requisito 2
