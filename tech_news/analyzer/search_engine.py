b460c81ff50d39b450e3482c83d1442414f703b5
Elienay Alves <elienay8486@gmail.com>
2023-04-24 00:27:42 -0300

    Requisitos 7, 8 e 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,13 +1,37 @@
-# Requisito 7
+from tech_news.database import search_news
+from datetime import datetime
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    search = {"title": {"$regex": f"{title}", "$options": "i"}}
+    result = search_news(search)
+    return [(news["title"], news["url"]) for news in result]
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    """try:
+        date = datetime.fromisoformat(date)
+    except ValueError:
+        raise ValueError("Data Inválida")
+
+    format_date = date.strftime("%d/%m/%Y")
+    search = {"timestamp": format_date}
+
+    result = search_news(search)
+
+    return [(news["title"], news["url"]) for news in result]"""
+    try:
+        format_date = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    result = search_news({"timestamp": format_date})
+
+    return [(news["title"], news["url"]) for news in result]
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    result = search_news({"category": {"$regex": category, "$options": "i"}})
+    return [(news["title"], news["url"]) for news in result]
